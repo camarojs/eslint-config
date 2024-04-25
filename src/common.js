2ee4e7372a1d6a0b19d519a38f21dfa653a8517e
Dunn <wg0121@outlook.com>
2024-04-25 23:51:33 +0800

    feat: add more eslint rules

--- src/common.js
+++ src/common.js
@@ -3,10 +3,10 @@ const eslint = require("@eslint/js");
 const globals = require("globals");
 
 const customized = stylistic.configs.customize({
-    jsx: false,
-    semi: true,
-    quotes: "double",
     indent: 4,
+    jsx: false,
+    quotes: "double",
+    semi: true,
 });
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
@@ -14,14 +14,34 @@ module.exports = [
     customized,
     eslint.configs.recommended,
     {
-        rules: {
-            "no-var": "error",
-            "prefer-const": "error",
-        },
         languageOptions: {
             globals: {
                 ...globals.node,
             },
         },
+        rules: {
+            // #region Possible Problems
+            "array-callback-return": "error",
+            "no-await-in-loop": "error",
+            "no-duplicate-imports": "error",
+            "no-promise-executor-return": "error",
+            "no-self-compare": "error",
+            "no-template-curly-in-string": "error",
+            "no-unmodified-loop-condition": "error",
+            "no-unreachable-loop": "error",
+            "no-useless-assignment": "error",
+            "require-atomic-updates": "error",
+            // #endregion
+
+            // #region Suggestions
+            "camelcase": "error",
+            "dot-notation": "error",
+            "no-var": "error",
+            "prefer-const": "error",
+            "sort-keys": ["error", "asc", { allowLineSeparatedGroups: true }],
+            "symbol-description": "error",
+            // #endregion
+        },
+
     },
 ];
--- src/typescript.js
+++ src/typescript.js
@@ -4,17 +4,19 @@ const { parser } = require("typescript-eslint");
 module.exports = [
     {
         files: ["**/*.ts"],
+        languageOptions: {
+            parser,
+            parserOptions: {
+                // eslint-disable-next-line camelcase
+                EXPERIMENTAL_useProjectService: true,
+            },
+        },
         rules: {
             // TODO: wait for typescript-eslint support eslint@v9
             // ...configs.recommendedTypeChecked,
             // ...configs.strictTypeChecked,
             // ...configs.stylisticTypeChecked,
         },
-        languageOptions: {
-            parser,
-            parserOptions: {
-                EXPERIMENTAL_useProjectService: true,
-            },
-        },
+
     },
 ];
