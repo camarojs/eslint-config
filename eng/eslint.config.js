cb5fef06a1e8988644bffef6df3de220b9376ba4
Dunn <wg0121@outlook.com>
2025-05-16 17:45:52 +0800

    refactor: migrate ESLint configuration to TypeScript

--- eng/eslint.config.js
+++ eng/eslint.config.js
@@ -1,11 +1,17 @@
-const common = require("../src/common");
+const { ts } = require("..");
 const globals = require("globals");
 
 /** @type {import('eslint').Linter.Config[]} */
 module.exports = [
-    ...common,
+    ...ts,
     {
-        files: ["src/**/*.js", "eng/**/*.js"],
+        ignores: ["lib/**/*"],
+    },
+    {
+        files: ["eng/**/*.js"],
         languageOptions: { globals: { ...globals.node } },
+    },
+    {
+        files: ["src/**/*.ts"],
     },
 ];
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import stylistic from "@stylistic/eslint-plugin";
+import eslintJS from "@eslint/js";
+import { Linter } from "eslint";
+import eslintTS from "typescript-eslint";
+import type { TSESLint } from "@typescript-eslint/utils";
+
+const styleLint = stylistic.configs.customize({
+    indent: 4,
+    jsx: false,
+    quotes: "double",
+    semi: true,
+});
+
+export const common: Linter.Config[] = [
+    eslintJS.configs.recommended,
+    styleLint,
+];
+
+const eslintTsRules = [
+    ...eslintTS.configs.recommendedTypeChecked,
+    ...eslintTS.configs.strictTypeChecked,
+    ...eslintTS.configs.stylisticTypeChecked,
+].reduce<TSESLint.FlatConfig.Rules>((acc, config) => ({ ...acc, ...config.rules }), {});
+
+export const ts: TSESLint.FlatConfig.ConfigArray = [
+    ...common,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: eslintTS.parser,
+            parserOptions: { projectService: true },
+        },
+        plugins: { "@typescript-eslint": eslintTS.plugin },
+        rules: { ...eslintTsRules },
+    },
+];
